0e293a43a28e017da752e76f5f7c8e1aa6518637
Vika <vika.karamyan@gmail.com>
2020-09-20 19:40:40 +0400

    Beverage package

---
+++ src/Beverage/Drink.java
@@ -0,0 +1,25 @@
+package Beverage;
+
+public abstract class Drink {
+    protected String name;
+    protected double volume;
+    protected float price;
+
+    public Drink(String name, double volume, float price) {
+        this.name = name;
+        this.volume = volume;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
